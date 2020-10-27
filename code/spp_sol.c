9c3e5384e6f7bb6c625debbf6f67c215554b0a65
wyatt.wu <1139728514@qq.com>
2020-10-28 01:50:15 +0800

    [FIX] fix the bug of read eph all the time. wyatt.wu

--- code/spp_sol.c
+++ code/spp_sol.c
@@ -14,10 +14,15 @@ RETURN_STATUS spp_proc(opt_file_t *opt_file)
 	uint8_t		  is_open_obs_file = false;	// false: obs file has been opened; true: has not been opened.
 	uint8_t		  is_open_nav_file = false;
 	uint8_t		  is_run           = true;
+	uint8_t		  is_fist_run	   = true;
 	while (is_run)
 	{
 		load_curr_rinex_obs(opt_file->obs_file, &obs_c, &is_open_obs_file, &is_run);
-		read_rinex_nav_data(opt_file->nav_file, &eph,   &is_open_nav_file);
+		if (is_fist_run)
+		{
+			read_rinex_nav_data(opt_file->nav_file, &eph, &is_open_nav_file);
+			is_fist_run = false;
+		}
 	}
 	
 
